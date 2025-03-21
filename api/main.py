6597bc1bd75a6149fb665f637bc4ab6932520784
Victor Rodrigues <victorrodrigues20@users.noreply.github.com>
2025-03-20 21:05:53 -0300

    Implementação da API

--- api/main.py
+++ api/main.py
@@ -1,14 +1,26 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class InfoPrevisao(BaseModel):
+    empresa : str
+    volume : float
+    prev_fecham : float
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def principal():
+    return {"status" : True,
+            "mensagem": "Serviço rodando"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/previsoes")
+def previsoes(infoPrevisao : InfoPrevisao):
+
+    if infoPrevisao.empresa == "aapl":
+        w0, w1, w2 = [-15.364324084645187, 1.0639578280039346, -3.2354336426203774e-09]
+        previsao = w0 + w1 * infoPrevisao.prev_fecham + w2 * infoPrevisao.volume
+    else:
+        previsao = None
+
+    return { "profecia" : previsao }
