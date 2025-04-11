0990413ee572fb3727f99a71f5c20c1e7b468148
Victor Rodrigues <victorrodrigues20@users.noreply.github.com>
2025-04-10 21:22:15 -0300

    teste params

--- site/streamlit_app.py
+++ site/streamlit_app.py
@@ -28,7 +28,7 @@ payload = {
 
 if st.button("Fazer previsão"):
     # Fazendo a requisição GET
-    response = requests.get(url, body=payload)
+    response = requests.post(url, params=payload)
 
     # Verificando se a requisição foi bem-sucedida
     if response.status_code == 200:
